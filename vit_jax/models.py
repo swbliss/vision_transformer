9390985619e84f8a947175e86df749f21c8704df
정석우 <swbliss1201@gmail.com>
2021-10-14 15:26:36 +0900

    Add VisionTransformer3D

--- vit_jax/models.py
+++ vit_jax/models.py
@@ -298,4 +298,96 @@ class VisionTransformer(nn.Module):
     return x
 
 
+class VisionTransformer3D(nn.Module):
+  """VisionTransformer3D."""
+
+  num_classes: int
+  patches: Any
+  transformer: Any
+  hidden_size: int
+  resnet: Optional[Any] = None
+  representation_size: Optional[int] = None
+  classifier: str = 'token'
+
+  @nn.compact
+  def __call__(self, inputs, *, train):
+
+    x = inputs
+    # (Possibly partial) ResNet root.
+    if self.resnet is not None:
+      width = int(64 * self.resnet.width_factor)
+
+      # Root block.
+      x = models_resnet.StdConv(
+          features=width,
+          kernel_size=(7, 7),
+          strides=(2, 2),
+          use_bias=False,
+          name='conv_root')(
+              x)
+      x = nn.GroupNorm(name='gn_root')(x)
+      x = nn.relu(x)
+      x = nn.max_pool(x, window_shape=(3, 3), strides=(2, 2), padding='SAME')
+
+      # ResNet stages.
+      if self.resnet.num_layers:
+        x = models_resnet.ResNetStage(
+            block_size=self.resnet.num_layers[0],
+            nout=width,
+            first_stride=(1, 1),
+            name='block1')(
+                x)
+        for i, block_size in enumerate(self.resnet.num_layers[1:], 1):
+          x = models_resnet.ResNetStage(
+              block_size=block_size,
+              nout=width * 2**i,
+              first_stride=(2, 2),
+              name=f'block{i + 1}')(
+                  x)
+
+    n, t, h, w, c = x.shape
+
+    # We can merge s2d+emb into a single conv; it's the same.
+    x = nn.Conv(
+        features=self.hidden_size,
+        kernel_size=self.patches.size,
+        strides=self.patches.size,
+        padding='VALID',
+        name='embedding')(
+            x)
+
+    # Here, x is a grid of embeddings.
+
+    # Transformer.
+    n, t, h, w, c = x.shape
+    x = jnp.reshape(x, [n, t * h * w, c])
+
+    # If we want to add a class token, add it here.
+    if self.classifier == 'token':
+      cls = self.param('cls', nn.initializers.zeros, (1, 1, c))
+      cls = jnp.tile(cls, [n, 1, 1])
+      x = jnp.concatenate([cls, x], axis=1)
+
+    x = Encoder(name='Transformer', **self.transformer)(x, train=train)
+
+    if self.classifier == 'token':
+      x = x[:, 0]
+    elif self.classifier == 'gap':
+      x = jnp.mean(x, axis=list(range(1, x.ndim - 1)))  # (1,) or (1,2)
+    else:
+      raise ValueError(f'Invalid classifier={self.classifier}')
+
+    if self.representation_size is not None:
+      x = nn.Dense(features=self.representation_size, name='pre_logits')(x)
+      x = nn.tanh(x)
+    else:
+      x = IdentityLayer(name='pre_logits')(x)
+    
+    if self.num_classes:
+      x = nn.Dense(
+        features=self.num_classes,
+        name='head',
+        kernel_init=nn.initializers.zeros)(x)
+    return x
+
 MlpMixer = models_mixer.MlpMixer
