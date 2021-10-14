67d026168b402cfd3321fe51e300d8eaadba013f
정석우 <swbliss1201@gmail.com>
2021-10-14 16:06:39 +0900

    Add 3d model config

--- vit_jax/configs/models.py
+++ vit_jax/configs/models.py
@@ -187,6 +187,14 @@ def get_b32_config():
   config.patches.size = (32, 32)
   return config
 
+@_register
+def get_3d_b32_config():
+  """Returns the ViT-B/32 configuration."""
+  config = get_b16_config()
+  config.name = 'ViT3D-B_32'
+  config.patches.size = (32, 32, 3)
+  return config
+
 
 @_register
 def get_l32_config():
